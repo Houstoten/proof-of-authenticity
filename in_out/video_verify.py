9a4e62c020f1a60978b0fe04026fc9b8c9105339
sergii-tk <sergii.tkachov@outlook.com>
2022-09-18 05:03:29 +0200

    separate as func

--- in_out/video_verify.py
+++ in_out/video_verify.py
@@ -3,24 +3,26 @@ import hashlib
 import pickle
 
 
-input_path = "cam_video"
+def video_verify(input_path):
 
-#open the files to check hashes with originals
-video = cv2.VideoCapture(input_path + ".mp4")
-hashfile = open(input_path + '.videohash','r')
-lasthash = "" #initial value, good for the code below, and posiibilty to add initial hash from the main network
-result = True
-while(video.isOpened()):
-    ret,frame = video.read()
-    if frame is None:
-        break
-    lasthash = hashlib.sha256((str(pickle.dumps(frame)) + lasthash).encode('utf-8')).hexdigest() #process the frame, combine hash from previous frame
-    if hashfile.readline().replace('\n','') != lasthash:
-        result = False
-        break
+    #open the files to check hashes with originals
+    video = cv2.VideoCapture(input_path + ".mp4")
+    hashfile = open(input_path + '.videohash','r')
+    lasthash = "" #initial value, good for the code below, and posiibilty to add initial hash from the main network
+    result = True
+    while(video.isOpened()):
+        ret,frame = video.read()
+        if frame is None:
+            break
+        lasthash = hashlib.sha256((str(pickle.dumps(frame)) + lasthash).encode('utf-8')).hexdigest() #process the frame, combine hash from previous frame
+        if hashfile.readline().replace('\n','') != lasthash:
+            result = False
+            break
 
 
-#close the hash and video files; report result
-video.release()
-hashfile.close()
-print(result)
+    #close the hash and video files; report result
+    video.release()
+    hashfile.close()
+    return result
+
+video_verify("cam_video")
--- in_out/webcam_capture.py
+++ in_out/webcam_capture.py
@@ -2,47 +2,47 @@ import cv2
 import pickle
 import hashlib
 
-#desired path. can be a input arg
-output_path = "cam_video"
+def webcam_capture(output_path):
+
+    #get the video stream from device 0 (main webcam) and configure all the stuff
+    video = cv2.VideoCapture(0)
+    frame_width = int(video.get(3))
+    frame_height = int(video.get(4))
+    vid_cod = cv2.VideoWriter_fourcc(*"hvc1")
+    output = cv2.VideoWriter(output_path + ".mp4", vid_cod, 20.0, (frame_width,frame_height))
 
 
-#get the video stream from device 0 (main webcam) and configure all the stuff
-video = cv2.VideoCapture(0)
-frame_width = int(video.get(3))
-frame_height = int(video.get(4))
-vid_cod = cv2.VideoWriter_fourcc(*"hvc1")
-output = cv2.VideoWriter(output_path + ".mp4", vid_cod, 20.0, (frame_width,frame_height))
+    #record until "X" key is hit
+    #for performance reason, and lack of time in this demo we just write to file and make hash later
+    while(True):
+        ret,frame = video.read()
+        cv2.imshow("Video capture demo. Press X to stop", frame)
+        output.write(frame)
+        if cv2.waitKey(1) &0XFF == ord('x'):
+            break
 
 
-#record until "X" key is hit
-#for performance reason, and lack of time in this demo we just write to file and make hash later
-while(True):
-    ret,frame = video.read()
-    cv2.imshow("Video capture demo. Press X to stop", frame)
-    output.write(frame)
-    if cv2.waitKey(1) &0XFF == ord('x'):
-        break
+    #close the video and file streams
+    video.release()
+    output.release()
+    cv2.destroyAllWindows()
 
 
-#close the video and file streams
-video.release()
-output.release()
-cv2.destroyAllWindows()
+    #reopen the file to make hashes (since video stream was compressed it will not match to the original frames before the compression)
+    video = cv2.VideoCapture(output_path + ".mp4")
+    outputfile = open(output_path + '.videohash','w')
+    lasthash = "" #initial value, good for the code below, and posiibilty to add initial hash from the main network
+    while(video.isOpened()):
+        ret,frame = video.read()
+        if frame is None:
+            break
+        lasthash = hashlib.sha256((str(pickle.dumps(frame)) + lasthash).encode('utf-8')).hexdigest() #process the frame, combine hash from previous frame
+        outputfile.write(lasthash + '\n')
 
 
-#reopen the file to make hashes (since video stream was compressed it will not match to the original frames before the compression)
-video = cv2.VideoCapture(output_path + ".mp4")
-outputfile = open(output_path + '.videohash','w')
-lasthash = "" #initial value, good for the code below, and posiibilty to add initial hash from the main network
-while(video.isOpened()):
-    ret,frame = video.read()
-    if frame is None:
-        break
-    lasthash = hashlib.sha256((str(pickle.dumps(frame)) + lasthash).encode('utf-8')).hexdigest() #process the frame, combine hash from previous frame
-    outputfile.write(lasthash + '\n')
+    #release the video stream and close the file
+    video.release()
+    outputfile.close()
 
 
-#release the video stream and close the file
-video.release()
-outputfile.close()
-
+webcam_capture("cam_video")
